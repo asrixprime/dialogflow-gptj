b01d0f2914e053587e40cf0d5d6d9c086e9ccfe7
Abu <ashaikh.saad.as@gmail.com>
2022-11-12 12:45:59 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# dialogflow-gptj
+# dialogflow and GPT3 Integration
+API written using nodejs for GPT3 connection with Dialogflow.
+
+## Steps to install on Windows:
+1) If using anaconda use following command to install nodje and npm in ve
+```
+conda create -n nodejs-env nodejs 
+```
+
+2) Clone the following repo with all the dependencies:
+```
+git clone https://github.com/abuelgasimsaadeldin/dialogflow-gpt3.git  
+```
+
+3) Create .env file and place following: 
+```
+OPENAI_API_KEY=""
+```
+
+4) Install the dependencies:
+```
+npm install
+```
+
+5) Run the API:
+```
+node dialogflowgpt3.js
+```
+
+6) Use ngrok to make localhost public
+```
+ngrok http 3000
+```
---
+++ dialogflowgptj.js
@@ -0,0 +1,61 @@
+const express = require("express");
+require("actions-on-google")
+require('dotenv').config();
+const axios = require('axios');
+const { WebhookClient } = require("dialogflow-fulfillment");
+const app = express();
+const fetch = require('node-fetch');
+
+app.get('/', (req,res) => {
+  res.send('Welcome to Dialogflow-GPTJ Application!!');
+})
+
+app.post("/dialogflow", express.json(), (req, res) => {
+    const agent = new WebhookClient({ request: req, response: res });
+    let intentMap = new Map();
+    intentMap.set("Default Welcome Intent", welcome);
+    intentMap.set("Default Fallback Intent", defaultFallback);
+    agent.handleRequest(intentMap);
+  
+    function welcome(agent) {
+      agent.add('Hi, I am Debra! I am your virtual personal assistant from Orient Telecoms. How are you doing today?');
+  }
+  
+  async function defaultFallback(agent) {
+      const instance = axios.create({
+        API_URL: 'https://api-inference.huggingface.co/models/EleutherAI/gpt-j-6B',
+        headers: { Authorization: `Bearer ${process.env.API_TOKEN}` },
+      });
+    
+      const dialog = [
+        `The following is a conversation with an AI assistant that can have meaningful conversations with users. The assistant is helpful, empathic, and friendly. Its objective is to make the user feel better by feeling heard. With each response, the AI assisstant prompts the user to continue the conversation in a natural way.
+
+        AI: Hello, my name is Debra. I am your personal AI assistant from Orient Telecoms. How are you doing today?`,
+      ];
+      let query = agent.query;
+      console.log('querytext ', query)
+      dialog.push(`User: ${query}`);
+      dialog.push('AI:');
+
+      try {
+        const response = await fetch(
+          'https://api-inference.huggingface.co/models/EleutherAI/gpt-j-6B',
+        {
+          headers: { Authorization: `Bearer ${process.env.API_TOKEN}` },
+          method: "POST",
+          body: JSON.stringify(query),
+        }
+      );
+        const botResponse = await response.json();
+        console.log('botResponse: ', botResponse[0].generated_text.substring(botResponse[0].generated_text.indexOf(':') + 1).trim())
+        // console.log('botResponse: ', botResponse)
+        agent.add(botResponse[0].generated_text.substring(botResponse[0].generated_text.indexOf(':') + 1).trim());
+      } catch (err) {
+        console.log('This is error:', err);
+        agent.add('Sorry. Something went wrong. Can you say that again?');
+      }
+    }
+  });
+
+const port = 3000;
+app.listen(port, () => console.log(`App listening on port ${port}!`))
